0e0d7bf3e7905dc8648cff40de2dc64b94a448ed
puia06 <puia.calin06@yahoo.ro>
2023-05-23 14:09:47 +0300

    Added Any and AnyTests classes RedPhase AnyTests

---
+++ Json.Start/AbstractionPolymorphismProject/Any.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionPolymorphismProject
+{
+    public class Any : IPattern
+    {
+        public Any(string accepted)
+        {
+            ...
+    }
+
+        public IMatch Match(string text)
+        {
+            ....
+        }
+    }
+}
---
+++ Json.Start/AbstractionPolymorphismProject/AnyTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionPolymorphismProject
+{
+    public class AnyTests
+    {
+        [Fact]
+        public void Match_FirstTextCharacterMatch_AnyFirstCharacter_ShouldReturnTrueAndRemainingText()
+        {
+            var e = new Any("eE");
+
+            string test = "ea";
+            var result = e.Match(test);
+
+            Assert.True(result.Success());
+            Assert.Equal("cd", result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_SecondTextCharacterMatch_AnyFirstCharacter_ShouldReturnTrueAndRemainingText()
+        {
+            var e = new Any("eE");
+
+            string test = "Ea";
+            var result = e.Match(test);
+
+            Assert.True(result.Success());
+            Assert.Equal("a", result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_TextCharacterMatch_DifferentAnyCharacters_ShouldReturnFalseAndRemainingText()
+        {
+            var e = new Any("eE");
+
+            string test = "a";
+            var result = e.Match(test);
+
+            Assert.False(result.Success());
+            Assert.Equal("a", result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_NullTextCharacterMatch_AnyCharacters_ShouldReturnFalseAndRemainingText()
+        {
+            var e = new Any("eE");
+
+            string test = null;
+            var result = e.Match(test);
+
+            Assert.True(result.Success());
+            Assert.Equal(null, result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_EmptyTextCharacterMatch_AnyCharacters_ShouldReturnFalseAndRemainingText()
+        {
+            var e = new Any("eE");
+
+            string test = "";
+            var result = e.Match(test);
+
+            Assert.True(result.Success());
+            Assert.Equal("", result.RemainingText());
+        }
+    }
+}
