de14c0c0d6ecb778b4dd4a52efe9f16dee6f9988
puia06 <puia.calin06@yahoo.ro>
2023-05-30 13:51:52 +0300

    Text class updated Number class - implementation, not working with all tests

--- Json.Start/AbstractionPolymorphismProject/Number.cs
+++ Json.Start/AbstractionPolymorphismProject/Number.cs
@@ -13,6 +13,14 @@ namespace AbstractionPolymorphismProject
 
         public Number()
         {
+            var digits = new Range('0', '9');
+            var nonZeroDigits = new Range('1', '9');
+            var integerPart = new Choice(new Character('0'), new Sequence(nonZeroDigits, new Many(digits)));
+            var decimalPart = new Optional(new Sequence(new Character('.'), new OneOrMore(digits)));
+            var exponent = new Optional(new Sequence(new Any("eE"), new Optional(new Any("+-")), new OneOrMore(digits)));
+
+            pattern = new Sequence(new Optional(new Character('-')), integerPart, decimalPart, exponent);
+
         }
 
         public IMatch Match(string text)
--- Json.Start/AbstractionPolymorphismProject/Text.cs
+++ Json.Start/AbstractionPolymorphismProject/Text.cs
@@ -12,29 +12,13 @@ namespace AbstractionPolymorphismProject
 
         public IMatch Match(string text)
         {
+
             if (string.IsNullOrEmpty(text) || prefix == null)
             {
                 return new Match(false, text);
             }
-            /*
-            foreach(char c in prefix)
-            {
-                if (!text.StartsWith(c))
-                {
-                    return new Match(false, text);
-                }
 
-                text = text[1..];
-            }
-            return new Match(true, text);
-            */
-
-            string textPrefix = "";
-            if (text.Length >= prefix.Length)
-            {
-                 textPrefix = text.Substring(0, prefix.Length);
-            }
-            if (textPrefix.Equals(prefix))
+            if (text.StartsWith(prefix))
             {
                 return new Match(true, text[prefix.Length..]);
             }
