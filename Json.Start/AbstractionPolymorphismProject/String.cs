b7723dc4c91f708d2280a167fa55980b525672e5
puia06 <puia.calin06@yahoo.ro>
2023-06-07 15:47:06 +0300

    Update String class in order to validate large Unicode characters *New empty Value and ValueTests classes

--- Json.Start/AbstractionPolymorphismProject/String.cs
+++ Json.Start/AbstractionPolymorphismProject/String.cs
@@ -13,10 +13,12 @@ namespace AbstractionPolymorphismProject
         public String()
         {
             var letter = new Range('a', 'z');
-            var hexaNumber = new Any("0123456789abcdefABCDEF");
-            var validHexaSequence = new Sequence(new Character('u'), hexaNumber, hexaNumber, hexaNumber, hexaNumber);
+            var validLargeUnicode = new Range('\u0120', '\uFFFF');
+            var emptySpace = new Character('\u0020');
+            var hexDigit = new Choice(new Range('0', '9'), new Range('a', 'f'), new Range('A', 'F'));
+            var validHexaSequence = new Sequence(new Character('u'), hexDigit, hexDigit, hexDigit, hexDigit);
             var controlCharacter = new Sequence(new Character('\\'), new Choice(new Any("bfnrt/\"\\"), validHexaSequence));
-            pattern = new Sequence(new Character('\"'), new OneOrMore(new Choice(letter, controlCharacter, new Character(' '))), new Character('\"'));
+            pattern = new Sequence(new Character('\"'), new OneOrMore(new Choice(letter, controlCharacter, emptySpace, validLargeUnicode)), new Character('\"'));
         }
 
         public IMatch Match(string text)
