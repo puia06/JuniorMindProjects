82603628ce8123d78033753c74a4bc80aa449567
puia06 <puia.calin06@yahoo.ro>
2023-06-08 12:32:05 +0300

    ValueTests & Value classes - first implementation try (not working for object and array)

--- Json.Start/AbstractionPolymorphismProject/Value.cs
+++ Json.Start/AbstractionPolymorphismProject/Value.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,41 @@ namespace AbstractionPolymorphismProject
 {
     public class Value
     {
+        private readonly IPattern pattern;
+
+        public Value()
+        {
+            var stringg = new String();
+            var wss = new Choice(new Text(""), new Character('\u0020'), new Character('\u000A'), new Character('\u000D'), new Character('\u0009'));
+            var ws = new Choice(
+             new Text(""),
+             new Sequence(new Character('\u0020'), wss),
+             new Sequence(new Character('\u000A'), wss),
+             new Sequence(new Character('\u000D'), wss),
+             new Sequence(new Character('\u0009'), wss)
+         );
+            var element = new Sequence(ws, pattern, ws);
+            var member = new Sequence(ws, stringg, ws, new Character(':'), element);
+            var members = new Choice(member, new Sequence(member, new Character(','), new List(member, new Character(','))));
+            var elements = new Choice(new Sequence(element, new Character(','), new List(element, new Character(','))), element);
+
+            var objectt = new Sequence(new Character('{'), new Choice(ws, members), new Character('}'));
+            var array = new Sequence(new Character('['), new Choice(ws, elements), new Character(']'));
+
+            pattern = new Choice(
+      objectt,
+      array,
+      new String(),
+      new Number(),
+      new Text("true"),
+      new Text("false"),
+      new Text("null")
+  );
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
     }
 }
--- Json.Start/AbstractionPolymorphismProject/ValueTests.cs
+++ Json.Start/AbstractionPolymorphismProject/ValueTests.cs
@@ -8,5 +8,82 @@ namespace AbstractionPolymorphismProject
 {
     public class ValueTests
     {
+        [Fact]
+        public void Match_trueValue_ShouldReturnTrue()
+        {
+            var value = new Value();
+            var testJson = "true";
+            var result = value.Match(testJson);
+
+            Assert.True(result.Success());
+            Assert.Equal("", result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_falseValue_ShouldReturnTrue()
+        {
+            var value = new Value();
+            var testJson = "false";
+            var result = value.Match(testJson);
+
+            Assert.True(result.Success());
+            Assert.Equal("", result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_nullValue_ShouldReturnTrue()
+        {
+            var value = new Value();
+            var testJson = "null";
+            var result = value.Match(testJson);
+
+            Assert.True(result.Success());
+            Assert.Equal("", result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_StringValue_ShouldReturnTrue()
+        {
+            var value = new Value();
+            var testJson = "\"abc\"";
+            var result = value.Match(testJson);
+
+            Assert.True(result.Success());
+            Assert.Equal("", result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_NumberValue_ShouldReturnTrue()
+        {
+            var value = new Value();
+            var testJson = "7";
+            var result = value.Match(testJson);
+
+            Assert.True(result.Success());
+            Assert.Equal("", result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_ArrayValue_ShouldReturnTrue()
+        {
+            var value = new Value();
+            var testJson = "[true, \"abc\"]";
+            var result = value.Match(testJson);
+
+            Assert.True(result.Success());
+            Assert.Equal("", result.RemainingText());
+        }
+
+        [Fact]
+        public void Match_ObjectValue_ShouldReturnTrue()
+        {
+            var value = new Value();
+
+            var testJson = " ";
+            var result = value.Match(testJson);
+
+            Assert.True(result.Success());
+            Assert.Equal("", result.RemainingText());
+        }
     }
 }
