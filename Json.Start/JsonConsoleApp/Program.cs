92968b76cd34eec4360281cad122bffceefae75e
puia06 <puia.calin06@yahoo.ro>
2023-06-13 12:29:05 +0300

    Added JsonConsoleApp class that allows checking whether a text entered from the keyboard is JSON or not.

---
+++ Json.Start/JsonConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using AbstractionPolymorphismProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Introdu un text pe a afla daca este un JSON valid");
+        string line;
+        string textJson = "";
+        while ((line = Console.ReadLine()) != string.Empty)
+        {
+            textJson += line;
+        }
+
+        Value value = new Value();
+
+        if (value.Match(textJson).Success() && value.Match(textJson).RemainingText() == "")
+        {
+            Console.WriteLine("Textul introdus este un Json valid");
+        }
+        else
+        {
+            Console.WriteLine("Textul introdus nu este un Json valid");
+        }
+        Console.ReadKey();
+    }
+}
