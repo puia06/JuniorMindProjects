e9468ea156568ed3afc25f3177f805de94f98528
puia06 <puia.calin06@yahoo.ro>
2023-07-12 10:59:50 +0300

    ObjectArray implementation ( should be in last commit) ObjectArrayTests GreenPhase

--- Json.Start/DataCollectionsImp/ObjectArray.cs
+++ Json.Start/DataCollectionsImp/ObjectArray.cs
@@ -6,7 +6,84 @@ using System.Threading.Tasks;
 
 namespace DataCollectionsImp
 {
-    internal class ObjectArray
+    public class ObjectArray
     {
+        protected object[] array;
+
+        public ObjectArray()
+        {
+            this.array = new object[4];
+        }
+
+        public void Add(object element)
+        {
+            array[Count] = element;
+            Count++;
+        }
+
+        public int Count { get; private set; } = 0;
+
+        public object this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        public bool Contains(object element)
+        {
+            return IndexOf(element) > -1;
+        }
+
+        public int IndexOf(object element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, object element)
+        {
+            Count++;
+            ResizeArrayIfNeeded();
+            for (int i = Count - 1; i >= index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+            array[index] = element;
+        }
+
+        public void Clear()
+        {
+            Array.Resize(ref array, 0);
+            Count = 0;
+        }
+
+        public void Remove(object element)
+        {
+            RemoveAt(IndexOf(element));
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            Count--;
+        }
+
+        private void ResizeArrayIfNeeded()
+        {
+            if (Count == array.Length)
+            {
+                Array.Resize(ref array, array.Length * 2);
+            }
+        }
     }
 }
---
+++ Json.Start/DataCollectionsImp/ObjectArrayTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollectionsImp
+{
+    public class ObjectArrayTests
+    {
+        [Fact]
+        public void Add_IndStringDouble_ShouldReturnTrue()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add(1);
+            testArray.Add("string");
+            testArray.Add(2.2);
+            Assert.Equal(1, testArray[0]);
+            Assert.Equal("string", testArray[1]);
+            Assert.Equal(2.2, testArray[2]);
+        }
+
+        [Fact]
+        public void SetElement_CorrectElement_ShouldReturnTrue()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add("string");
+            testArray.Add(2);
+            testArray[0] = 2.2;
+
+            Assert.Equal(2.2, testArray[0]);
+        }
+
+        [Fact]
+        public void Contains_Element_ShouldReturnTrue()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add("string");
+            testArray.Add(2);
+
+            Assert.True(testArray.Contains("string"));
+        }
+
+        [Fact]
+        public void Contains_ElementNotContained_ShouldReturnFalse()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add(1);
+            testArray.Add(2);
+
+            Assert.False(testArray.Contains("string"));
+        }
+
+        [Fact]
+        public void IndexOf_ElementPresent_ShouldReturnTrue()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add(1);
+            testArray.Add(2.2);
+            testArray.Add("string");
+
+            Assert.Equal(2, testArray.IndexOf("string"));
+        }
+
+        [Fact]
+        public void IndexOf_ElementNotPresent_ShouldReturnFalse()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add(1);   
+            testArray.Add(2);
+
+            Assert.Equal(-1, testArray.IndexOf("string"));
+        }
+
+
+        [Fact]
+        public void Insert_InsertElement_ShouldReturnTrue()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add(1);
+            testArray.Add(2);
+            testArray.Insert(1, 9.9);
+
+            Assert.Equal(9.9, testArray[1]);
+        }
+
+        [Fact]
+        public void Clear_ClearElements_ShouldReturnTrue()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add(1);
+            testArray.Add("string");
+            testArray.Clear();
+
+            Assert.Equal(0, testArray.Count);
+        }
+
+        [Fact]
+        public void Remove_RemoveElement_ShouldReturnTrue()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add("string");
+            testArray.Add(2.2);
+            testArray.Add(3);
+            testArray.Remove("string");
+
+            Assert.Equal(2.2, testArray[0]);
+            Assert.Equal(3, testArray[1]);
+            Assert.Equal(2, testArray.Count);
+        }
+
+        [Fact]
+        public void RemoveAt_RemoveElement_ShouldReturnTrue()
+        {
+            var testArray = new ObjectArray();
+            testArray.Add("string");
+            testArray.Add(2);
+            testArray.RemoveAt(0);
+
+            Assert.Equal(2, testArray[1]);
+        }
+    }
+}
