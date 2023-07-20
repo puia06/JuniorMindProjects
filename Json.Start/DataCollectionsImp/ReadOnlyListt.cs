3d578a48bad4b7803d911f98f70b3d978ddc6982
puia06 <puia.calin06@yahoo.ro>
2023-07-20 10:39:57 +0300

    Added ReadOnlyTests - GreenPhase

--- Json.Start/DataCollectionsImp/ReadOnlyListt.cs
+++ Json.Start/DataCollectionsImp/ReadOnlyListt.cs
@@ -1,12 +1,82 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DataCollectionsImp
 {
-    internal class ReadOnlyListt
-    {
+    public class ReadOnlyListt<T> : IList<T> 
+    { 
+        private IList<T> readOnlyList;
+
+        public ReadOnlyListt(IList<T> readOnlyList)
+        {
+            this.readOnlyList = readOnlyList;
+        }
+
+        public int Count { get; private set; } = 0;
+
+        public bool IsReadOnly { get; set; } = true;
+
+        public T this[int index]
+        {
+            get
+            {
+                return readOnlyList[index];
+            }
+             set => throw new NotSupportedException("IList is ReadOnly!"); }
+
+        public void Add(T item)
+        {
+            throw new NotSupportedException("IList is ReadOnly!");
+        }
+        public bool Contains(T element)
+        {
+            return IndexOf(element) > -1;
+        }
+
+        public void Clear()
+        {
+            throw new NotSupportedException("IList is ReadOnly!");
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotSupportedException("IList is ReadOnly!");
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return readOnlyList.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            return readOnlyList.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotSupportedException("IList is ReadOnly!");
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotSupportedException("IList is ReadOnly!");
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotSupportedException("IList is ReadOnly!");
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
---
+++ Json.Start/DataCollectionsImp/ReadOnlyListtTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollectionsImp
+{
+    public class ReadOnlyListtTests
+    {
+        [Fact]
+        public void Add_ShouldReturnException()
+        {
+            var testList = new Listt<int>();
+            testList.Add(1);
+            testList.Add(2);
+            var readOnlyList = new ReadOnlyListt<int>(testList);
+
+            Assert.Throws<NotSupportedException>(() => readOnlyList.Add(1));
+        }
+
+        [Fact]
+        public void Contains_ShouldReturnTrue()
+        {
+            var testList = new Listt<int>();
+            testList.Add(1);
+            testList.Add(2);
+            var readOnlyList = new ReadOnlyListt<int>(testList);
+
+            Assert.True(readOnlyList.Contains(1));
+        }
+
+        [Fact]
+        public void Set_ShouldReturnException()
+        {
+            var testList = new Listt<int>();
+            testList.Add(1);
+            testList.Add(2);
+            var readOnlyList = new ReadOnlyListt<int>(testList);
+
+            Assert.Throws<NotSupportedException>(() => readOnlyList[0] = 1);
+        }
+
+        [Fact]
+        public void Clear_ShouldReturnException()
+        {
+            var testList = new Listt<int>();
+            testList.Add(1);
+            testList.Add(2);
+            var readOnlyList = new ReadOnlyListt<int>(testList);
+
+            Assert.Throws<NotSupportedException>(() => readOnlyList.Clear());
+        }
+
+
+        [Fact]
+        public void IndexOf_ShouldReturnTrue()
+        {
+            var testList = new Listt<int>();
+            testList.Add(1);
+            testList.Add(2);
+            var readOnlyList = new ReadOnlyListt<int>(testList);
+
+            Assert.Equal(0, readOnlyList.IndexOf(1));
+        }
+
+        [Fact]
+        public void Insert_ShouldReturnException()
+        {
+            var testList = new Listt<int>();
+            testList.Add(1);
+            testList.Add(2);
+            var readOnlyList = new ReadOnlyListt<int>(testList);
+
+            Assert.Throws<NotSupportedException>(() => readOnlyList.Insert(1, 2));
+        }
+
+
+        [Fact]
+        public void Remove_ShouldReturnException()
+        {
+            var testList = new Listt<int>();
+            testList.Add(1);
+            testList.Add(2);
+            var readOnlyList = new ReadOnlyListt<int>(testList);
+
+            Assert.Throws<NotSupportedException>(() => readOnlyList.Remove(1));
+        }
+
+        [Fact]
+        public void RemoveAt_ShouldReturnException()
+        {
+            var testList = new Listt<int>();
+            testList.Add(1);
+            testList.Add(2);
+            var readOnlyList = new ReadOnlyListt<int>(testList);
+
+            Assert.Throws<NotSupportedException>(() => readOnlyList.RemoveAt(1));
+        }
+
+        [Fact]
+        public void CopyTo_ShouldReturnException()
+        {
+            var testList = new Listt<int>();
+            testList.Add(1);
+            testList.Add(2);
+            var readOnlyList = new ReadOnlyListt<int>(testList);
+            var arr = new int[2];
+
+            Assert.Throws<NotSupportedException>(() => readOnlyList.CopyTo(arr, 1));
+        }
+    }
+}
