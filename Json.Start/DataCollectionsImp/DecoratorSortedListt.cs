f7eb964284600e3eef74a4810fa1b99da9193f0e
puia06 <puia.calin06@yahoo.ro>
2023-07-20 14:53:18 +0300

    Decorator Pattern: ListtDecorator implements IListt. DecoratorSortedListt extends ListtDecorator, adding SortArray() method to the Listt class methods that modifies the array

---
+++ Json.Start/DataCollectionsImp/DecoratorSortedListt.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollectionsImp
+{
+    internal class DecoratorSortedListt<T> : ListtDecorator<T> where T : IComparable<T>
+    {
+        private IList<T> sortedListt;
+        public DecoratorSortedListt(IList<T> sortDescendingList) : base(sortDescendingList)
+        {
+            sortedListt = sortDescendingList;
+        }
+
+        public override T this[int index]
+        {
+            set
+            {
+                sortedListt[index] = value;
+                SortArray();
+            }
+        }
+        public override void Add(T item)
+        {
+            sortedListt.Add(item);
+            SortArray();
+        }
+
+        public override void Insert(int index, T item)
+        {
+            sortedListt.Insert(index, item);
+            SortArray();
+        }
+
+        public override void RemoveAt(int index)
+        {
+            sortedListt.RemoveAt(index);
+            SortArray();
+        }
+        private void SortArray()
+        {
+            for (int i = 1; i < Count; i++)
+            {
+                for (int j = i; j > 0 && base[j - 1].CompareTo(base[j]) > 0; j--)
+                {
+                    T x = base[j];
+                    base[j] = base[j - 1];
+                    base[j - 1] = x;
+                }
+            }
+        }
+    }
+}
---
+++ Json.Start/DataCollectionsImp/ListtDecorator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollectionsImp
+{
+    public class ListtDecorator<T> : IList<T>
+    {
+        private IList<T> decoratorList;
+
+        public ListtDecorator(IList<T> printList)
+        {
+            this.decoratorList = printList;
+        }
+        public virtual T this[int index] 
+        {
+            get => decoratorList[index];
+            set => decoratorList[index] = value;
+        }
+
+        public int Count { get; private set; } = 0;
+
+        public bool IsReadOnly { get; } = false;
+
+        public virtual void Add(T item)
+        {
+            decoratorList.Add(item);
+        }
+        public bool Contains(T element)
+        {
+            return IndexOf(element) > -1;
+        }
+
+        public void Clear()
+        {
+            decoratorList.Clear();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            decoratorList.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return decoratorList.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            return decoratorList.IndexOf(item);
+        }
+
+        public virtual void Insert(int index, T item)
+        {
+           decoratorList.Insert(index, item);   
+        }
+
+        public bool Remove(T item)
+        {
+            return decoratorList.Remove(item);
+        }
+
+        public virtual void RemoveAt(int index)
+        {
+            decoratorList.RemoveAt(index);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
